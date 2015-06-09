d13b7a2a5831bc0bb2065c67421391497af54f3a
Jake Avery <javery@siliconmind.com>
2015-06-09 13:41:29 -0400

    Removed font awesome. Added chevrons to accordian.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 $(document).foundation();
+
+/* Custom JS ***************************************************************************/
+
+// Accordion jump to top and animate
+$(document).foundation('accordion', {
+    callback: function (el){
+        var containerPos = $(el).parent().offset().top;
+        $('html, body').animate({scrollTop: containerPos}, 300);
+    }
+});
+
+// Rotate chevrons on accordion
+// Needs to be based on active state of panels
+/*$('.accordion-navigation a').click(function(){
+    $('img', this).toggleClass('down');
+});*/
